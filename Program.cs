0bd0a75cb0f04c10114a47e45f2c306433ae478a
Adam-Dodson00 <62917191+Adam-Dodson00@users.noreply.github.com>
2021-03-11 16:34:38 +1100

    initial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Selectiontasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string user = "Adam";
+            string pass = "1234";
+
+            Console.WriteLine("Please enter username");
+            string username =  Console.ReadLine();
+
+            if (username == user){
+                Console.WriteLine("Please enter password");
+                string password = Console.ReadLine();
+
+                if (password == pass){
+                    Console.WriteLine("Login Successful");
+                }
+                else{ 
+                    Console.WriteLine("Login Unsuccessful");
+                }
+            }
+            else{
+                Console.WriteLine("Login Unsuccessful");
+            }  
+        }
+    }
+}
